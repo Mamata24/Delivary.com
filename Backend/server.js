41574f32503935e4721116b203556af927b13cad
Mamata24 <nmamta240@gmail.com>
2020-11-02 19:32:27 +0530

    BACKEND-server.js

---
+++ Backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const dotenv = require('dotenv')
+
+const userRouter = require('./Routes/userRouter')
+
+dotenv.config()
+const app = express()
+require('./mongoose')
+
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.use(userRouter)
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`)
+})
