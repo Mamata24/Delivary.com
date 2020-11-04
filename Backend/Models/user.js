34a88098d9950341c43c7f700dd7b39a8fb0f692
Akki <gakhila281994@gmail.com>
2020-11-04 23:26:56 +0530

    Merged

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -1,81 +1,82 @@
-const mongoose = require('mongoose')
-const validator = require('validator')
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     first_name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     last_name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
         }
+      },
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password shouldnt contain the word password')
-            }
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 7,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password shouldnt contain the word password");
         }
+      },
     },
-}, {
+  },
+  {
     versionKey: false,
-}
-)
+  }
+);
 
 userSchema.methods.toJSON = function () {
-    const user = this
-    const userObject = user.toObject()
+  const user = this;
+  const userObject = user.toObject();
 
-    delete userObject.password
+  delete userObject.password;
 
-    return userObject
-}
+  return userObject;
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email })
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw new Error('Unable to login')
-    }
+  if (!user) {
+    throw new Error("Unable to login");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password)
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error('Unable to login')
-    }
-    return user
-}
+  if (!isMatch) {
+    throw new Error("Unable to login");
+  }
+  return user;
+};
 
-userSchema.pre('save', async function (next) {
-    const user = this
+userSchema.pre("save", async function (next) {
+  const user = this;
 
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    }
+  next();
+});
 
-    next()
-})
+const User = mongoose.model("User", userSchema);
 
-const User = mongoose.model('User', userSchema)
-
-module.exports = User
+module.exports = User;
