0422ad0f63da98b6db9f1c084e6bbe8b6a346265
Mamata24 <nmamta240@gmail.com>
2020-11-04 13:20:58 +0530

    Added CORS

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const dotenv = require('dotenv')
 
+const cors = require('cors')
+
 const userRouter = require('./Routes/userRouter')
 
 dotenv.config()
@@ -11,6 +13,8 @@ require('./mongoose')
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+
+app.use(cors())
 
 app.use(userRouter)
 
