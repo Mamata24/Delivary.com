704179c6351611fd0f8dfe1aff253d583d6df008
Mamata24 <nmamta240@gmail.com>
2020-11-05 21:40:03 +0530

    BACKEND-server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 const userRouter = require('./Routes/userRouter')
 const restaurantRouter = require('./Routes/restaurantRouter')
+const menuRouter = require('./Routes/menuRouter')
 
 dotenv.config()
 const app = express()
@@ -19,6 +20,7 @@ app.use(cors())
 
 app.use(userRouter)
 app.use(restaurantRouter)
+app.use(menuRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`)
