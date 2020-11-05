e168395aca96fc042cf597f6c87d4828a8d884de
Mamata24 <nmamta240@gmail.com>
2020-11-05 11:00:51 +0530

    Updated - mongoose.js

--- Backend/mongoose.js
+++ Backend/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
-// const dotenv = require('dotenv')
-
-// dotenv.config()
-
+const restaurants = require('./restaurants')
+const Restaurant = require('./Models/restaurant')
+const { db } = require('./Models/restaurant')
 //Mongoose setup to create connection with DB
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
     if (err) {
@@ -13,3 +12,17 @@ mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useCreateIndex:
     }
 }
 )
+
+// mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
+//     if (err) console.log("Connection to the database failed")
+//     else {
+//         db.collection("restaurants").countDocuments(function (err, count) {
+//             if (count === 0) {
+//                 Restaurant.insertMany(restaurants)
+//                     .then((res) => console.log(res))
+//                     .catch((err) => console.log(err))
+//             }
+//         });
+//         console.log("Connected to the database successfully")
+//     }
+// });
