8595f0f2d892fd2dc3c1bd2b657bbddaf8e45b90
Mamata24 <nmamta240@gmail.com>
2020-11-05 21:41:34 +0530

    BACKEND- mongoose.js

--- Backend/mongoose.js
+++ Backend/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const restaurants = require('./restaurants')
 const Restaurant = require('./Models/restaurant')
+const menu = require('./menu')
+const Menu = require('./Models/menu')
 const { db } = require('./Models/restaurant')
 //Mongoose setup to create connection with DB
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true }, (err) => {
@@ -19,6 +21,20 @@ mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useCreateIndex:
 //         db.collection("restaurants").countDocuments(function (err, count) {
 //             if (count === 0) {
 //                 Restaurant.insertMany(restaurants)
+//                     .then((res) => console.log(res))
+//                     .catch((err) => console.log(err))
+//             }
+//         });
+//         console.log("Connected to the database successfully")
+//     }
+// });
+
+// mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (err) => {
+//     if (err) console.log("Connection to the database failed")
+//     else {
+//         db.collection("menu").countDocuments(function (err, count) {
+//             if (count === 0) {
+//                 Menu.insertMany(menu)
 //                     .then((res) => console.log(res))
 //                     .catch((err) => console.log(err))
 //             }
