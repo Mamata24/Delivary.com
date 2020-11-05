079edc21e3a05988114c9c3d2d45be13362dfd95
Mamata24 <nmamta240@gmail.com>
2020-11-05 10:56:19 +0530

    Removed files

--- Backend/Controllers/getRestaurants.js
+++
@@ -1,9 +0,0 @@
-const Restaurant = require('../Models/user')
-exports.getRestaurant = async (req, res) => {
-    try {
-        const restaurant = await Restaurant.find({})
-        res.send(restaurant)
-    } catch (e) {
-        res.status(500).send(e)
-    }
-}
