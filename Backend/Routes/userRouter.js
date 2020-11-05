5a390348e307cd44fec3e7da93987efc28146935
Mamata24 <nmamta240@gmail.com>
2020-11-05 10:59:54 +0530

    Updated- ROUTER - userRouter

--- Backend/Routes/userRouter.js
+++ Backend/Routes/userRouter.js
@@ -4,7 +4,6 @@ const User = require('../Models/user')
 const addUsers = require('../Controllers/addUser')
 const getUsers = require('../Controllers/getUser')
 
-
 router.post('/signup', addUsers.addUser)
 router.post('/login', getUsers.getUser)
 module.exports = router
