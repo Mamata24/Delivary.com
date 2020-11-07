27514c22289512ae9e7cf20cdeb19190ab16d836
Mamata24 <nmamta240@gmail.com>
2020-11-07 18:34:37 +0530

    UPDATED - BACKEND - Implemented OAuth to the register page

--- Backend/Controllers/addUser.js
+++ Backend/Controllers/addUser.js
@@ -1,11 +1,18 @@
 const User = require("../Models/user");
 exports.addUser = async (req, res) => {
-  const user = new User(req.body);
-  console.log(user);
-  try {
-    await user.save();
-    res.status(201).send({ user });
-  } catch (e) {
-    res.status(400).send(e);
+  // const user = (await User.findOne({ email: req.body.email }))
+  // console.log(user)
+  if (!await User.findOne({ email: req.body.email })) {
+    const user = new User(req.body);
+    console.log(user);
+    try {
+      await user.save();
+      res.status(201).send({ user });
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  }
+  else {
+    res.status(201).send()
   }
 };
