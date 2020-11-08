e9ba1df5ad5dcb01298c1dc4cac467b383368105
Mamata24 <nmamta240@gmail.com>
2020-11-08 22:10:52 +0530

    Added- Menu display component

--- Frontend/src/Menu/Menu.jsx
+++ Frontend/src/Menu/Menu.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import DashboardNav from "../Dashboard/DashboardNav";
+import MenuDisplay from "./MenuDisplay";
 import StoreDetailHeader from "./StoreDetailHeader";
 import SubMenu from "./SubMenu";
 
 function Menu() {
-  return(
+  return (
     <>
       <div className="container-fluid">
         <div className="row">
-            <DashboardNav />
-            <SubMenu />
-            <StoreDetailHeader />
+          <DashboardNav />
+          <SubMenu />
+          <StoreDetailHeader />
+          <div className="row">
+            <div className="col-lg-8">
+              <MenuDisplay />
+            </div>
+            <div className="col-lg-4">
+
+            </div>
+          </div>
         </div>
       </div>
     </>
