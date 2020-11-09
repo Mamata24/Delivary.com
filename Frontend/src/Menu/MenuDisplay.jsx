2bbec00697890ab50ec50e378041aead54c1a063
Mamata24 <nmamta240@gmail.com>
2020-11-09 17:31:28 +0530

    Updated Menu Display - Fetched data for menu

--- Frontend/src/Menu/MenuDisplay.jsx
+++ Frontend/src/Menu/MenuDisplay.jsx
@@ -1,67 +1,70 @@
 import React from "react";
+import MenuData from './MenuData.json'
 function MenuDisplay() {
-    return (
-        <>
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-lg-3">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a
-                                    class="nav-link active"
-                                    data-toggle="collapse"
-                                    href="#appetiser"
-                                    aria-expanded="false"
-                                    aria-controls="appetiser"
-                                >
-                                    Appetiser
-                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a
-                                    class="nav-link"
-                                    data-toggle="collapse"
-                                    href="#breakFast"
-                                    aria-expanded="false"
-                                    aria-controls="breakFast"
-                                >
-                                    Break Fast
-                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a
-                                    class="nav-link"
-                                    data-toggle="collapse"
-                                    href="#lunch"
-                                    aria-expanded="false"
-                                    aria-controls="lunch"
-                                >
-                                    Lunch
-                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a
-                                    class="nav-link"
-                                    data-toggle="collapse"
-                                    href="#dinner"
-                                    aria-expanded="false"
-                                    aria-controls="dinner"
-                                >
-                                    Dinner
-                </a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="col-lg-9" style={{ backgroundColor: "white" }}>
-                        <p className="mt-3">
-                            <a
-                                data-toggle="collapse"
-                                href="#appetiser"
-                                role="button"
-                                aria-expanded="false"
-                                aria-controls="collapseExample"
-                            >
-                                Appetiser
+  const categorySet = ["Appetizer", "Breakfast"]
+  return (
+    <>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-lg-3">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  data-toggle="collapse"
+                  href="#appetiser"
+                  aria-expanded="false"
+                  aria-controls="appetiser"
+                >
+                  Appetizer
+                  </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  data-toggle="collapse"
+                  href="#breakFast"
+                  aria-expanded="false"
+                  aria-controls="breakFast"
+                >
+                  Break Fast
+                   </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  data-toggle="collapse"
+                  href="#lunch"
+                  aria-expanded="false"
+                  aria-controls="lunch"
+                >
+                  Main Course
+                   </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link"
+                  data-toggle="collapse"
+                  href="#dinner"
+                  aria-expanded="false"
+                  aria-controls="dinner"
+                >
+                  Pudding
+                   </a>
+              </li>
+
+            </ul>
+          </div>
+          <div className="col-lg-9" style={{ backgroundColor: "white" }}>
+            <p className="mt-3">
+              <a
+                data-toggle="collapse"
+                href="#appetiser"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+              >
+                Appetiser
                 <span style={{ float: "right" }}>
                   <i class="fas fa-angle-down"></i>
                 </span>
@@ -72,115 +75,180 @@ function MenuDisplay() {
                 <div className="row">
                   <div className="col-lg-6">
                     {/* filter */}
-                    <div className="row">
-                      <div className="col-lg-8">
-                        <a href="#chicken">Chicken Briyani</a>
+                    {MenuData.filter(data => data.category == 'Appetizer' && data.restaurant_id == 1 && data.dish_id < 6).map(filteredAppetizer =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredAppetizer.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredAppetizer.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredAppetizer.dish_price}</p>
+                        </div>
                       </div>
-                      <div className="col-lg-4">
-                        <p>$12.00</p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-lg-8">
-                        <a href="#chicken">Chicken Briyani</a>
-                      </div>
-                      <div className="col-lg-4">
-                        <p>$12.00</p>
-                      </div>
-                    </div>
-                    <div className="row">
-                      <div className="col-lg-8">
-                        <a href="#chicken">Chicken Briyani</a>
-                      </div>
-                      <div className="col-lg-4">
-                        <p>$12.00</p>
-                      </div>
-                    </div>
+                    )}
                   </div>
                   <div className="col-lg-6">
-                  <div className="row">
-                      <div className="col-lg-8">
-                        <a href="#chicken">Fish Kabab</a>
+                    {MenuData.filter(data => data.category == 'Appetizer' && data.restaurant_id == 1 && data.dish_id > 5).map(filteredAppetizer =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredAppetizer.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredAppetizer.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredAppetizer.dish_price}</p>
+                        </div>
                       </div>
-                      <div className="col-lg-4">
-                        <p>$20.00</p>
-                      </div>
-                    </div>
+                    )}
                   </div>
                 </div>
               </div>
-                        </div>
-                        <p>
-                            <a
-                                data-toggle="collapse"
-                                href="#breakFast"
-                                role="button"
-                                aria-expanded="false"
-                                aria-controls="collapseExample"
-                            >
-                                Break Fast
-                <span style={{ float: "right" }}>
-                                    <i class="fas fa-angle-down"></i>
-                                </span>
-                            </a>
-                        </p>
-                        <div class="collapse" id="breakFast">
-                            <div class="card card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                accusamus terry richardson ad squid. Nihil anim keffiyeh
-                                helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                                ea proident.
-              </div>
-                        </div>
-                        <p>
-                            <a
-                                data-toggle="collapse"
-                                href="#lunch"
-                                role="button"
-                                aria-expanded="false"
-                                aria-controls="collapseExample"
-                            >
-                                Lunch
-                <span style={{ float: "right" }}>
-                                    <i class="fas fa-angle-down"></i>
-                                </span>
-                            </a>
-                        </p>
-                        <div class="collapse" id="lunch">
-                            <div class="card card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                accusamus terry richardson ad squid. Nihil anim keffiyeh
-                                helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                                ea proident.
-              </div>
-                        </div>
-                        <p>
-                            <a
-                                data-toggle="collapse"
-                                href="#dinner"
-                                role="button"
-                                aria-expanded="false"
-                                aria-controls="collapseExample"
-                            >
-                                Dinner
-                <span style={{ float: "right" }}>
-                                    <i class="fas fa-angle-down"></i>
-                                </span>
-                            </a>
-                        </p>
-                        <div class="collapse" id="dinner">
-                            <div class="card card-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                accusamus terry richardson ad squid. Nihil anim keffiyeh
-                                helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                                ea proident.
-              </div>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </>
-    );
+            <p>
+              <a
+                data-toggle="collapse"
+                href="#breakFast"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+              >
+                Break Fast
+                <span style={{ float: "right" }}>
+                  <i class="fas fa-angle-down"></i>
+                </span>
+              </a>
+            </p>
+            <div class="collapse" id="breakFast">
+              <div class="card card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    {/* filter */}
+                    {MenuData.filter(data => data.category == 'Breakfast' && data.restaurant_id == 2 && data.dish_id < 36).map(filteredBreakfast =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredBreakfast.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredBreakfast.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredBreakfast.dish_price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="col-lg-6">
+                    {MenuData.filter(data => data.category == 'Breakfast' && data.restaurant_id == 2 && data.dish_id > 35).map(filteredBreakfast =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredBreakfast.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredBreakfast.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredBreakfast.dish_price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>
+              <a
+                data-toggle="collapse"
+                href="#lunch"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+              >
+                Main Course
+                <span style={{ float: "right" }}>
+                  <i class="fas fa-angle-down"></i>
+                </span>
+              </a>
+            </p>
+            <div class="collapse" id="lunch">
+              <div class="card card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    {/* filter */}
+                    {MenuData.filter(data => data.category == 'Main Course' && data.restaurant_id == 1 && data.dish_id < 46).map(filteredMainCourse =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredMainCourse.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredMainCourse.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredMainCourse.dish_price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="col-lg-6">
+                    {MenuData.filter(data => data.category == 'Main Course' && data.restaurant_id == 1 && data.dish_id > 45).map(filteredMainCourse =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredMainCourse.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredMainCourse.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredMainCourse.dish_price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p>
+              <a
+                data-toggle="collapse"
+                href="#dinner"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapseExample"
+              >
+                Pudding
+                <span style={{ float: "right" }}>
+                  <i class="fas fa-angle-down"></i>
+                </span>
+              </a>
+            </p>
+            <div class="collapse" id="dinner">
+              <div class="card card-body">
+                <div className="row">
+                  <div className="col-lg-6">
+                    {/* filter */}
+                    {MenuData.filter(data => data.category == 'Pudding' && data.restaurant_id == 2 && data.dish_id < 76).map(filteredPudding =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredPudding.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredPudding.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredPudding.dish_price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                  <div className="col-lg-6">
+                    {MenuData.filter(data => data.category == 'Pudding' && data.restaurant_id == 2 && data.dish_id > 75).map(filteredPudding =>
+                      <div className="row">
+                        <div className="col-lg-8">
+                          <a href="#chicken">{filteredPudding.dish_name}</a>
+                          <div style={{ color: "grey" }}>{filteredPudding.dish_description}</div>
+                        </div>
+                        <div className="col-lg-4">
+                          <p>{filteredPudding.dish_price}</p>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default MenuDisplay;
